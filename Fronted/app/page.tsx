6541e75ebe30eef641056341c02c606a9af6c6b0
anuprajvarma <anupraj1854@gmail.com>
2025-04-26 12:43:27 +0530

    made signup ui

--- Fronted/app/page.tsx
+++ Fronted/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+export default function SignUp() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      todolist
+    <div className="w-screen h-screen bg-background flex justify-center">
+      <div className="w-[30rem] bg-background flex items-center justify-center">
+        <div className="w-[25rem] h-[30rem] bg-CardBackground flex justify-center items-center">
+          <div className="h-[15rem] w-[15rem] flex flex-col gap-8">
+            <h1 className="text-4xl text-center">Sign Up</h1>
+            <div className="flex flex-col gap-2">
+              <label>Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="border border-amber-100"
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                id="pwd"
+                name="pwd"
+                className="border border-amber-100"
+              />
+              <input type="submit" value="Submit" className="cursor-pointer" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
