94005377ed6e417db7004065ef3181b83d09f1e8
anuprajvarma <anupraj1854@gmail.com>
2025-05-04 14:13:53 +0530

    make todo page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,32 @@
 export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      akjdfhsld
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-[40rem] h-[60rem] flex flex-col gap-2 justify-center items-center">
+        <div className="w-[30rem] h-[40rem] flex flex-col justify-center items-center">
+          <div>
+            <h1 className="text-center">Anupraj Todo-List</h1>
+            <form className="flex justify-between">
+              <input
+                className="w-[14rem] bg-white"
+                type="search"
+                name="search"
+                required
+              />
+              <button>Add</button>
+            </form>
+            <div className="p-2 flex gap-2">
+              <input type="checkbox" name="checkbox" />
+              <p>complete learning nextjs 15</p>
+              <button>edit</button>
+            </div>
+            <hr />
+            <div className="p-2 flex gap-2">
+              <input type="checkbox" name="checkbox" />
+              <p>deleted todos</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
