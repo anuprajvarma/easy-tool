f15fc015777bdbad0d6e47148956f56a7587278d
anuprajvarma <anupraj1854@gmail.com>
2025-05-04 15:06:57 +0530

    complete login page

---
+++ client/app/login/page.tsx
@@ -0,0 +1,32 @@
+const Login = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="w-[60rem] h-screen flex flex-col gap-2  items-center">
+        <div className="w-[40rem] h-[40rem] flex flex-col">
+          <h1>Login Page</h1>
+          <form className="flex flex-col" method="POST" action="/user/login">
+            <label>email</label>
+            <input
+              type="text"
+              required
+              name="email"
+              placeholder="enter your email"
+            />
+            <label>password</label>
+            <input
+              type="text"
+              required
+              name="password"
+              placeholder="enter your password"
+            />
+            <button className="cursor-pointer" type="submit">
+              submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
