847a675adeb6e0e003b7f96843aac1e3334a2ede
anuprajvarma <anupraj1854@gmail.com>
2025-05-04 13:07:13 +0530

    delete all file and folder for oauth

--- Fronted/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div className="w-screen h-screen bg-background flex justify-center">
-      <div className="w-[30rem] bg-background flex items-center justify-center">
-        Home Page
-      </div>
-    </div>
-  );
-}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,25 +1,7 @@
-import { auth } from "@/app/auth";
-import { redirect } from "next/navigation";
-import SignOutPage from "@/components/sing-out";
-
 export default async function Home() {
-  const session = await auth();
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {!session ? (
-        redirect("/signin")
-      ) : (
-        <>
-          <h1>
-            Welcome, {session.user?.name || "User"}!__
-            {session.user?.email || "User"}
-          </h1>
-          <div>
-            <SignOutPage></SignOutPage>
-          </div>
-        </>
-      )}
+      akjdfhsld
     </div>
   );
 }
