b42feed0d8c3b036ba530401809876c204d32342
anuprajvarma <anupraj1854@gmail.com>
2025-05-05 14:37:37 +0530

    connect client and server

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,20 @@
-export default async function Home() {
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:5001/api/todolist")
+      .then((res) => res.json())
+      .then((data) => setName(data.message));
+  }, []);
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="w-[40rem] h-screen flex flex-col gap-2 justify-center items-center">
         <div className="w-[30rem] h-[40rem] flex flex-col justify-center items-center">
           <div>
-            <h1 className="text-center">Anupraj Todo-List</h1>
+            <h1 className="text-center">{name} Todo-List</h1>
             <form className="flex justify-between">
               <input
                 className="w-[14rem] bg-gray-700 text-white"
--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow Next.js frontend to access this API
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(
+    "mongodb+srv://anupraj1854:18NK3fCAO4UoBPAS@todocluster.9ag2fzh.mongodb.net/?retryWrites=true&w=majority&appName=todocluster"
+  )
+  .then(console.log("mongodb connected"));
+
+app.get("/api/todolist", (req, res) => {
+  console.log("request accesss");
+  res.json({ message: "Anupraj" });
+});
 
 app.listen(PORT, () => console.log("server is started"));
