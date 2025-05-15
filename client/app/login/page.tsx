c8c25e922a3f47946c2ee1e6826fd756b5d8ceb0
anuprajvarma <anupraj1854@gmail.com>
2025-05-15 19:08:44 +0530

    make api for login auth

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,16 +1,42 @@
+"use client";
+
+import { useState } from "react";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await fetch("http://localhost:5001/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(formData as Record<string, string>).toString(),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="w-[60rem] h-screen flex flex-col gap-2  items-center">
         <div className="w-[40rem] h-[40rem] flex flex-col">
           <h1>Login Page</h1>
-          <form className="flex flex-col" method="POST" action="/user/login">
+          <form className="flex flex-col" onSubmit={handleSubmit}>
             <label>email</label>
             <input
               type="text"
               required
               name="email"
               placeholder="enter your email"
+              onChange={handleChange}
             />
             <label>password</label>
             <input
@@ -18,6 +44,7 @@ const Login = () => {
               required
               name="password"
               placeholder="enter your password"
+              onChange={handleChange}
             />
             <button className="cursor-pointer" type="submit">
               submit
--- server/index.js
+++ server/index.js
@@ -22,11 +22,20 @@ mongoose
 
 app.post("/api/signup", async (req, res) => {
   const { name, email, password } = req.body;
+  console.log(email);
   const user = await User.create({
     name,
     email,
     password,
   });
+  res.json(user);
+});
+
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  const user = await User.findOne({ email, password });
+  console.log(user);
   res.json(user);
 });
 
