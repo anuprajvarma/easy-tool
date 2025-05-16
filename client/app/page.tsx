b5b7c89994a0ab5dddbfc7fa57167313a5f81726
anuprajvarma <anupraj1854@gmail.com>
2025-05-16 18:29:32 +0530

    setup create todos and get on fronted

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,21 +3,44 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
+interface Todo {
+  id: string;
+  email: string;
+  title: string;
+}
+
+interface ApiResponse {
+  success: boolean;
+  redirectTo?: string;
+  todo: Todo[];
+  user: {
+    email: string;
+    name: string;
+  };
+}
+
 export default function Home() {
-  const [name, setName] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [todoTitle, setTodoTitle] = useState<string>("");
+  const [todoChange, setTodoChange] = useState<boolean>(true);
   const router = useRouter();
 
   useEffect(() => {
     const fetchTodoList = async () => {
       try {
         const res = await fetch("http://localhost:5001/api/todo", {
-          credentials: "include", // ✅ if you are using cookies
+          credentials: "include",
         });
-        const data = await res.json();
+        const data: ApiResponse = await res.json();
+        // console.log(`data ${data}`);
+
         if (data.success) {
           router.push(data.redirectTo);
         } else {
-          // console.log(data.user);
+          setTodos(data.todo);
+          setEmail(data.user.email);
           setName(data.user.name);
         }
       } catch (err) {
@@ -25,10 +48,33 @@ export default function Home() {
       }
     };
 
-    fetchTodoList(); // 👈 call the async function
-  }, []);
+    fetchTodoList();
+  }, [todoChange]);
 
-  const handleSubmit = () => {};
+  useEffect(() => {
+    console.log("Updated todos:", todos);
+  }, [todoChange]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoTitle(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    // console.log(todoTitle);
+    setTodoChange(!todoChange);
+    const res = await fetch("http://localhost:5001/api/todo", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({ todoTitle, email }).toString(),
+    });
+    const data = await res.json();
+    // console.log(data.todo);
+    setTodoTitle("");
+  };
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
@@ -39,24 +85,19 @@ export default function Home() {
             <form className="flex justify-between" onSubmit={handleSubmit}>
               <input
                 className="w-[14rem] bg-white text-black border border-gray-300"
-                type="search"
-                name="search"
+                type="text"
+                name="title"
                 required
+                value={todoTitle}
+                onChange={handleChange}
               />
               <button className="cursor-pointer" type="submit">
                 Add
               </button>
             </form>
-            <div className="p-2 flex gap-2">
-              <input type="checkbox" name="checkbox" />
-              <p>complete learning nextjs 15</p>
-              <button>edit</button>
-            </div>
-            <hr />
-            <div className="p-2 flex gap-2">
-              <input type="checkbox" name="checkbox" />
-              <p>deleted todos</p>
-            </div>
+            {todos.map((item, index) => {
+              return <div key={index}>{item.title}</div>;
+            })}
           </div>
         </div>
       </div>
--- server/routers/todo.js
+++ server/routers/todo.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const { loginRestrection } = require("../middlewares/auth");
+const Todo = require("../models/todo");
 
 const router = express.Router();
 
-// router.post("/todo", );
+router.post("/todo", async (req, res) => {
+  const { todoTitle, email } = req.body;
+  console.log(email, todoTitle);
+  const todo = await Todo.create({
+    title: todoTitle,
+    email,
+  });
+  res.json({ todo });
+});
 
 router.get("/todo", loginRestrection, async (req, res) => {
   const user = req.user;
-  res.json({ user });
+  // res.json({ user });
+  const todo = await Todo.find({ email: user.email });
+  console.log(todo);
+  res.json({ user, todo });
 });
 
 // router.delete("/todo", handleSignup);
