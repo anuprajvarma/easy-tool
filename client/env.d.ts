5288b535a7431f4a32522f954fdfe68de8a87942
anuprajvarma <anupraj1854@gmail.com>
2025-05-18 20:53:03 +0530

    delete one unused file

--- client/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
-    NEXTAUTH_SECRET: string;
-    NEXTAUTH_URL: string;
-  }
-}
